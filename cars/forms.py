c8e4ce0afde9fd395a1e8c1066b0de15cc9f8917
Fadhli Nur Himawan <fadhlinurhimawan@gmail.com>
2025-04-23 16:26:30 +0700

    feat: add tailwind and styling page

--- cars/forms.py
+++ cars/forms.py
@@ -2,11 +2,35 @@ from django import forms
 from .models import Car, Service
 
 class CarForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add Tailwind classes to all fields automatically
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            })
+    
     class Meta:
         model = Car
         fields = '__all__'
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 3}),
+            'is_electric': forms.CheckboxInput(attrs={'class': 'h-5 w-5'}),
+        }
 
+# cars/forms.py
 class ServiceForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            })
+    
     class Meta:
         model = Service
         fields = '__all__'
+        widgets = {
+            'notes': forms.Textarea(attrs={'rows': 3}),
+            'service_date': forms.DateInput(attrs={'type': 'date'}),
+        }
